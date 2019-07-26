104f91beeacf48ef744e80492dddb15b2cbc915a
june <kimjy4536@hotmail.com>
2019-07-26 19:42:34 +0900

    add home css

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,166 +1,67 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
+  Modal, TouchableHighlight,
+  View, Alert,
   StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+  Image,
+  Platform
 } from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import { observer } from 'mobx-react';
+import userStore from './../stores/UserStore';
 
-import { MonoText } from '../components/StyledText';
+@observer
+export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
+  _handleClick() {
+    // this.props.navigation.navigate('Chat')
+  }
+
+  render() {
+    return (
+      <Container>
+        <Content >
           <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
+            source={{ uri: 'https://yt3.ggpht.com/a/AGF-l78bW3omuJwQGhPI_sM8JrnwV-0ATQ4ctPiPrQ=s88-mo-c-c0xffffffff-rj-k-no' }}
+            style={styles.containerImg}
           />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
+          <Grid style={styles.containerButtonBox}>
+            <Col>
+              <Button block transparent style={styles.containerButton}>
+                <Icon active name='ios-heart' style={styles.containerIcon} />
+              </Button>
+            </Col>
+            <Col >
+              <Button block transparent style={styles.containerButton}>
+                <Icon active name='ios-chatboxes' style={styles.containerIcon} />
+              </Button>
+            </Col>
+            <Col >
+              <Button block transparent style={styles.containerButton}>
+                <Icon active name='md-information' style={styles.containerIcon} />
+              </Button>
+            </Col>
+          </Grid>
+          <Body style={styles.container}>
+            <Text style={styles.containerText}>This is just a transparent card with some text to boot.</Text>
+          </Body>
+        </Content>
+      </Container>
     );
   }
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
     position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
+    backgroundColor: '#fff',
+    height: 304,
+    top: 476,
+    flex: 1,
     ...Platform.select({
       ios: {
         shadowColor: 'black',
@@ -172,27 +73,31 @@ const styles = StyleSheet.create({
         elevation: 20,
       },
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    borderRadius: 12,
   },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+  containerImg: {
+    flex: 1,
+    width: '100%',
+    height: 380,
+    // resizeMode: 'contain'
   },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
+  containerButtonBox: {    
     alignItems: 'center',
   },
-  helpLink: {
-    paddingVertical: 15,
+  containerButton: {
+    height: 100
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  containerIcon: {
+    fontSize: 40,
+    color: 'red',
+  },
+  containerText: {
+    top: 30,
+    padding: 18,
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 15,
+    lineHeight: 14,
+    color: '#444444',
   },
 });
